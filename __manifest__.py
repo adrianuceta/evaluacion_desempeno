04c702a492bb3dc35ac43943d43031235005612d
adrianuceta <aucegam0210@g.educaand.es>
2025-01-28 08:44:07 +0100

    manifest, csv e imagen terminadas

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+# gestion_tareas/__manifest__.py
+{
+'name': 'Gestión de Tareas',
+'version': '1.0',
+'summary': 'Módulo para evaluar el desempeño individual de los empleados',
+'category': 'Productivity',
+'author': 'Adrián Uceta Gamaza',
+'website': 'https://tuweb.com',
+'license': 'LGPL-3',
+'depends': ['hr'],
+'icon': '/evaluacion_desempeno/static/description/icon53.png',
+'data': [
+'views/gestion_tarea_views.xml',
+'security/ir.model.access.csv',
+],
+'assets': {
+'web.assets_backend': [
+'evaluacion_desempeno/static/src/css/styles.css',
+],
+},
+'application': True,
+'installable': True,
+'auto_install': False,
+'description': """
+Módulo de Odoo para la evaluación de desempeño de empleados,
+incluyendo vistas Kanban y formulario detallado.
+"""
+}
