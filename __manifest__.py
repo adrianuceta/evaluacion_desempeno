ec9d56050a74097819aac5a88f577709ffa9d249
adrianuceta <aucegam0210@g.educaand.es>
2025-01-28 10:10:46 +0100

    Últimos cambios en manifest y xml para error en la fecha

--- __manifest__.py
+++ __manifest__.py
@@ -1,28 +1,27 @@
-# evaluacion_desempeno/__manifest__.py
 {
 'name': 'Evaluación de desempeño',
 'version': '1.0',
-'summary': 'Módulo para evaluar el desempeño individual de los empleados',
+'summary': 'Módulo para evaluar el desempeño de los empleados',
 'category': 'Human Resources',
 'author': 'Adrián Uceta Gamaza',
 'website': 'https://tuweb.com',
 'license': 'LGPL-3',
-'depends': ['base','hr'],
+'depends': ['base', 'mail', 'hr'],
 'icon': '/evaluacion_desempeno/static/description/icon53.png',
 'data': [
 'views/evaluacion_views.xml',
 'security/ir.model.access.csv',
 ],
-'assets': {
-'web.assets_backend': [
-'evaluacion_desempeno/static/src/css/styles.css',
-],
-},
 'application': True,
 'installable': True,
 'auto_install': False,
+'assets': {
+    'web.assets_backend': [
+        '/evaluacion_desempeno/static/src/css/styles.css',
+    ],
+},
 'description': """
-Módulo de Odoo para la evaluación de desempeño de empleados,
+Módulo de Odoo para la evaluación del desempeño de los empleados,
 incluyendo vistas Kanban y formulario detallado.
 """
 }
